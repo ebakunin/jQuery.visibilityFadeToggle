8c00db7a0c0f5ccf6b2c9cb0ee92e4021b09fe04
Eric Christenson <ebakunin@gmail.com>
2015-03-11 01:53:25 -0700

    Added complete option. Changed duration to match $.fadeIn() parameter.

--- jquery.fn.visibilityFadeToggle.js
+++ jquery.fn.visibilityFadeToggle.js
@@ -2,24 +2,40 @@
  * A version of jQuery.fadeToggle() that uses 'visibility' CSS styling
  *
  * @author   Eric Christenson
- * @date     February 22, 2015
- * @version  1.0
+ * @date     March 11, 2015
+ * @version  2.0
  * @license  MIT License (http://opensource.org/licenses/MIT)
  *
- * @param  fadeSpeed  int (fade-out time in milliseconds. Optional, defaults to 1 second)
+ * @param  duration  mixed (optional. Accepts time in milliseconds, 'slow', or 'fast')
+ * @param  complete  function (optional. A callback function)
  */
 (function ($) {
-    $.fn.visibilityFadeToggle = function (fadeSpeed) {
-        fadeSpeed = fadeSpeed || 1000;
+    $.fn.visibilityFadeToggle = function (duration, complete) {
+        var speed = duration || 400;
+        
+        switch (speed) {
+            case 'fast':
+                speed = 200;
+                break;
+            case 'slow':
+                speed = 600;
+                break;
+            default:
+                speed = parseInt(duration);
+        }
 
         $(this).each(function (index) {
             if ($(this).css('visibility') === 'hidden') {
-                $(this).delay(index * 100).css({opacity: 0, visibility: 'visible'}).fadeTo(parseInt(fadeSpeed), 1);
+                $(this).delay(index * 100).css({opacity: 0, visibility: 'visible'}).fadeTo(speed, 1);
             } else {
-                $(this).delay(index * 100).fadeTo(parseInt(fadeSpeed), 0, function () {
+                $(this).delay(index * 100).fadeTo(speed, 0, function () {
                     $(this).css({ visibility: 'hidden', opacity: 1 });
                 });
             }
+        });
+        
+        $(this).promise().done(function () {
+            complete();
         });
     }
 })(jQuery);
